b8f9970fc7d506eded41c84bb62fb03d709c5dbf
zhn-6 <zhanghanzhang495@gmail.com>
2026-08-05 16:27:26 +0800

    Update regions list in handler.mjs
    Removed several regions from the list and added 'hkg1'.

--- api/handler.mjs
+++ api/handler.mjs
@@ -9,24 +9,6 @@ export const config = {
   // Vercel Edge Network Regions
   // https://vercel.com/docs/regions#region-list
   regions: [
-    "arn1",
-    "bom1",
-    "cdg1",
-    "cle1",
-    "cpt1",
-    "dub1",
-    "dxb1",
-    "fra1",
-    "gru1",
-    //"hkg1",
-    "hnd1",
-    "iad1",
-    "icn1",
-    "kix1",
-    "lhr1",
-    "pdx1",
-    "sfo1",
-    "sin1",
-    "syd1",
+    "hkg1",
   ],
 };
